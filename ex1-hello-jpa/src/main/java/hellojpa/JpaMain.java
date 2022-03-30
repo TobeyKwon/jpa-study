7b3579c59b0d8d0c7e6a6ea1429e36620626da38
TobeyKwon <tobeykwon@gmail.com>
2022-03-30 23:38:09 +0900

    0330 jpa

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,12 +15,16 @@ public class JpaMain {
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
+
         try {
-//            Member member = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+
+            // 영속
+            Member member = em.find(Member.class, 100L);
+            member.setName("AAA");
+
+            em.detach(member);
+
+            System.out.println("======================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
